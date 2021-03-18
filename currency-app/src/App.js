9309f9716ab28e8cd60bbd5ed69a506ead042a17
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-17 21:18:13 -0400

    add axios / set up api

--- currency-app/src/App.js
+++ currency-app/src/App.js
@@ -1,9 +1,16 @@
+import * as api from './api'
+
 import CurrencyForm from './components/CurrencyForm'
 import Currencies from './components/Currencies'
+
+
 
 export default function App() {
   const onSubmit = (currencies) => {
     console.log('onSubmit:', currencies)
+    api.getConversion(currencies)
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
   }
 
   return (
