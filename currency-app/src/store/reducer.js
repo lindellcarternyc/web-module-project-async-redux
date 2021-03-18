d2f2d4efae23279f33fd4c0e2b95406dbca8398e
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-17 22:24:01 -0400

    implement reducer

--- currency-app/src/store/reducer.js
+++ currency-app/src/store/reducer.js
@@ -1,3 +1,5 @@
+import { FETCH_CONVERSION_ATTEMPT, FETCH_CONVERSION_SUCCESS, FETCH_CONVERSION_FAILURE } from './actions'
+
 const INITIAL_STORE_STATE = {
     conversion: null,
     loading: false,
@@ -6,6 +8,30 @@ const INITIAL_STORE_STATE = {
 
 export default function reducer(state = INITIAL_STORE_STATE, action) {
     switch (action.type) {
+        case FETCH_CONVERSION_ATTEMPT: {
+            return {
+                ...state,
+                conversion: null,
+                loading: true,
+                error: null
+            }
+        }
+        case FETCH_CONVERSION_SUCCESS: {
+            return {
+                ...state,
+                conversion: action.payload,
+                loading: false,
+                error: null
+            }
+        }
+        case FETCH_CONVERSION_FAILURE: {
+            return {
+                ...state,
+                conversion: null,
+                loading: false,
+                error: action.payload
+            }
+        }
         default: {
             return state
         }
